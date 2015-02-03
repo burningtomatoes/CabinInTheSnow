945ea4ccf1c95a16dfa67aada34d92e2308d4a45
Roy de Jong <roy@softwarepunt.nl>
2015-02-03 23:13:14 +0100

    [Content] Fireplace: Automatically stop burning after X amount of hours

--- src/map/entities/burningfireplace.js
+++ src/map/entities/burningfireplace.js
@@ -1,4 +1,8 @@
 var BurningFirePlace = Entity.extend({
+    HOURS_PER_LIGHTING: 6,
+
+    timeStarted: 0,
+
     init: function () {
         this._super();
 
@@ -10,7 +14,24 @@ var BurningFirePlace = Entity.extend({
         this.spriteHead = new Animation('fireplace_lit', this.width, this.height, 10, 3, true);
         this.direction = Direction.UP;
 
+        this.timeStarted = Time.timer;
+
         this.setCoord(10, 11);
         this.posY -= 18;
+    },
+
+    update: function () {
+        this._super();
+
+        var timeSince = Time.timer - this.timeStarted;
+        var hoursSince = Math.floor(timeSince / Time.SECONDS_PER_HOUR);
+
+        if (hoursSince >= this.HOURS_PER_LIGHTING) {
+            this.map.remove(this);
+            this.map.fireplaceLit = false;
+            this.map = null;
+        }
+
+        console.log('its been hrs: ' + hoursSince);
     }
 });
