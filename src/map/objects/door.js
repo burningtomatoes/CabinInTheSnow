065d9f42180d11d5cf3b0f59bfed496ba587a4d6
Roy de Jong <roy@softwarepunt.nl>
2015-02-04 22:48:35 +0100

    [Content] Door script: Fix issue where the game can totally crash and die when you spam SPACE on the door

--- src/map/objects/door.js
+++ src/map/objects/door.js
@@ -2,12 +2,21 @@
  * Door transport script.
  */
 window.mapObjects['door'] = MapObject.extend({
+    busy: false,
+
     trigger: function (player) {
+        if (this.busy) {
+            return;
+        }
+
+        this.busy = true;
         this.doTeleport(player);
     },
 
     doTeleport: function () {
-        Game.loadMap(this.target);
+        Game.loadMap(this.target, function () {
+            this.busy = false;
+        }.bind(this));
         Sfx.play('door_closing.wav');
     }
 });
